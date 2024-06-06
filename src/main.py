ece2f43de4e2d17adbc617de908ca98c3ee9b956
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2024-06-06 14:18:21 +0200

    Create a pdf for each Excel file

--- src/main.py
+++ src/main.py
@@ -1,19 +1,27 @@
 import pandas as pd
 import glob
+from fpdf import FPDF   
+from pathlib import Path
+    
 
 filepaths = glob.glob("invoices/*.xlsx")
-print("Filepaths found:", filepaths)
 
-
-if not filepaths:
-    print("No files found.")
-else:
-    for filepath in filepaths:
-        # Print the filepath being processed
-        print("Processing file:", filepath)
-        df = pd.read_excel(filepath, sheet_name="Sheet 1")
-        print(df)
-
-
-
-
+for filepath in filepaths:
+    # Print the filepath being processed
+    print("Processing file:", filepath)
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    
+    # create pdf file
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    
+    # extract filename and invoice number 
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+    
+    # add content to pdf
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
+        
+    
+pdf.output(f"PDFs/{filename}.pdf")
